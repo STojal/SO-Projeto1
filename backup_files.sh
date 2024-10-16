784cc8a3ee17a0b65a64251c5c97c7dbbade1a84
Tiago-0li-veira <tiago.oliveira1@ua.pt>
2024-10-16 23:51:11 +0100

    fase 1 quase acabada

--- backup_files.sh
+++ backup_files.sh
@@ -2,41 +2,69 @@
 
 Help(){
     echo "Run this script to create a backup for a directory."
-    echo
     echo "Syntax: ./backup.sh [-c] [-b tfile] [-r regexpr] working_dir backup_dir"
     echo "c     only see the output of the script without actually copying anything"
     echo "b     pass a txt file with the names of the files and directories to not be copied"
     echo "r     only copy the files and directories that match the regex expression"
 }
 
+check=false
+
+# parse options
+while getopts 'ch' opt; do
+    case $opt in
+    c)
+        echo "Processing option -c (dry run, no copying)"
+        check=true  # Set check to true when -c is passed
+        ;;
+    h)
+        Help
+        exit 0
+        ;;
+    ?)
+        echo "Error: invalid command option"
+        Help
+        exit 1
+        ;;
+    esac
+done
+shift "$(($OPTIND -1))"
+
 if [[ $# -ne 2 ]];then
-    # echo "Insira mais informacao para realizar o script"
+    echo "Error: invalid number of arguments"
     Help
     exit 1
 fi
 
+# argument variables
 pwd=$1
 backup_dir=$2
 
-if [[ -d $1 ]]; then
-    echo "$1 exists and is a directory!"
+# check if working directory exists
+if [[ -d $pwd ]]; then
+    echo "$pwd exists and is a directory!"
 else
-    echo "$1 is not a directory"
-    exit
+    echo "$pwd is not a directory"
+    exit 1
 fi
 
-if [[ -d $backup_dir ]];then
+# check if backup exists and create it in case it doesn't
+if [[ -d $backup_dir ]]; then
     echo "Backup directory is valid!"
 else
     echo "Creating backup directory..."
-    backup_dir=~/$backup_dir
-    mkdir $backup_dir
-    echo "$backup_dir directory was created succesfully!"
+    mkdir -p "$backup_dir"
+    echo "$backup_dir directory was created successfully!"
 fi
 
-for path in $pwd/*; do
+# loop through files in the source directory
+for path in "$pwd"/*; do
     if [[ -f $path ]]; then
-        $(cp $pwd/$path $backup_dir)
+        echo "cp $path $backup_dir"  # always show the command
+
+        # if check is false, actually copy the file
+        if [[ "$check" == false ]]; then
+            cp "$path" "$backup_dir"
+        fi
     fi
 done
-
